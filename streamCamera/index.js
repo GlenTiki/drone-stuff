1c908671739da69d47942b9335ddbe50054e311c
Glen Keane <glenkeane.94@gmail.com>
2015-06-10 16:36:50 +0100

    initialize repo

---
+++ streamCamera/index.js
@@ -0,0 +1,25 @@
+var http = require("http"),
+    drone = require("dronestream"),
+    arDrone = require("ar-drone");
+    client  = arDrone.createClient();
+
+var server = http.createServer(function(req, res) {
+  require("fs").createReadStream(__dirname + "/index.html").pipe(res);
+
+  client.takeoff();
+
+  client.stop();
+  .after(6000, function(){
+    console.log(this);
+    this.clockwise(0.5);
+  })
+  .after(3000, function(){
+    this.stop();
+    this.land();
+  });
+});
+
+drone.listen(server);
+server.listen(5555);
+
+
